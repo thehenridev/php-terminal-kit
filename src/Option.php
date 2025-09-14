f9bf5eaf9c11a3ea4320789cf1a55fed6ff42684
Henrique <carlos745k@gmail.com>
2025-09-14 14:54:06 -0300

    Criando a estrutura básica da função de criação de lista de seleção

---
+++ src/Option.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace TheHenriDev\PHPTerminalKit;
+
+class Option
+{
+  protected string $value;
+  protected bool $selected;
+  protected \stdClass $storage;
+
+  public function select()
+  {
+    $this->selected = true;
+  }
+
+  public function unselect()
+  {
+    $this->selected = false;
+  }
+
+  public function isSelected()
+  {
+    return $this->selected;
+  }
+
+  public function getValue()
+  {
+    return $this->value;
+  }
+
+  public function __set(string $property, $value)
+  {
+    $this->storage->$property = $value;
+  }
+
+  public function __construct(string $value)
+  {
+    $this->value = $value;
+    $this->selected = false;
+    $this->storage = new \stdClass;
+  }
+
+  public function __get(string $property)
+  {
+    return $this->storage->$property ?? null;
+  }
+}
---
+++ src/SelectionList.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace TheHenriDev\PHPTerminalKit;
+
+class SelectionList
+{
+  protected array $options;
+
+  public function add(array|string $options)
+  {
+    if(is_string($options)) {
+      $options = array($options);
+    }
+
+    foreach($options as $option) {
+      if(!is_string($option)) {
+        continue;
+      }
+
+      $this->options[] = new Option($option);
+    }
+
+    return $this;
+  }
+
+  public function del(array|string $options)
+  {
+    if(is_string($options)) {
+      $options = array($options);
+    }
+
+    foreach($options as $target) {
+      if(!is_string($target)) {
+        continue;
+      }
+
+      foreach($this->options as $key => $option) {
+        if($target === $option->getValue()) {
+          unset($this->options[$key]);
+        }
+      }
+    }
+
+    return $this;
+  }
+
+  public function getOptions()
+  {
+    return $this->options;
+  }
+
+  public function select(array|string $options)
+  {
+    if(is_string($options)) {
+      $options = array($options);
+    }
+
+    foreach($options as $target) {
+      if(!is_string($target)) {
+        continue;
+      }
+
+      foreach($this->options as $key => $option) {
+        if($target === $option->getValue()) {
+          $this->options[$key]->select();
+        }
+      }
+    }
+    
+    return $this;
+  }
+
+  public function unselect(array|string $options)
+  {
+    if(is_string($options)) {
+      $options = array($options);
+    }
+
+    foreach($options as $target) {
+      if(!is_string($target)) {
+        continue;
+      }
+
+      foreach($this->options as $key => $option) {
+        if($target === $option->getValue()) {
+          $this->options[$key]->unselect();
+        }
+      }
+    }
+    
+    return $this;
+  }
+
+  public function unselectAll()
+  {
+    foreach($this->options as $option) {
+      $option->unselect();
+    }
+    
+    return $this;
+  }
+
+  public function clear()
+  {
+    $this->options = array();
+    
+    return $this;
+  }
+
+  public function prettyPrint()
+  {
+    $pretty = array();
+    foreach($this->getOptions() as $option) {
+      array_push($pretty, sprintf("[%s] %s", ($option->isSelected() ? '*' : ' '), $option->getValue()));
+    }
+    
+    return implode("\n", $pretty);
+  }
+
+  public function __construct(array|string $options = [])
+  {
+    $this->options = array();
+    $this->add($options);
+  }
+}
